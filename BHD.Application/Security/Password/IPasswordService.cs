b679384ba2e7f49be11ac296f2da63e1c4383921
Walddry Gonzalez <Walddry@gmail.com>
2024-10-31 16:41:29 -0400

    Feat: PasswordService to handle the login and store the password in DB

---
+++ BHD.Application/Security/Password/IPasswordService.cs
@@ -0,0 +1,8 @@
+﻿namespace BHD.Application.Security.Password
+{
+    public interface IPasswordService
+    {
+        string HashPassword(string password);
+        bool VerifyPassword(string hashedPassword, string providedPassword);
+    }
+}
